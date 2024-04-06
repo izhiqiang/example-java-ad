bf0cdc12947394074c8e18107ce3215f380bf774
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-04-06 21:23:02 +0800

    基于 Ribbon 实现微服务调用

---
+++ ad-service/ad-search/src/main/java/com/example/ad/controller/SearchController.java
@@ -0,0 +1,41 @@
+package com.example.ad.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.ad.annotation.IgnoreResponseAdvice;
+import com.example.ad.client.vo.AdPlan;
+import com.example.ad.client.vo.AdPlanGetRequest;
+import com.example.ad.response.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class SearchController {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public SearchController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @SuppressWarnings("all")
+    @IgnoreResponseAdvice
+    @PostMapping("/getAdPlansByRibbon")
+    public CommonResponse<List<AdPlan>> getAdPlansByRebbon(
+            @RequestBody AdPlanGetRequest request
+    ) {
+        log.info("ad-search: getAdPlansByRibbon -> {}",
+                JSON.toJSONString(request));
+        return restTemplate.postForEntity(
+                "http://eureka-client-ad-sponsor/ad-sponsor/get/adPlan",
+                request,
+                CommonResponse.class
+        ).getBody();
+    }
+}
