ff4155af1cf9457fcdc19340197d0bd08216de7e
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-04-07 11:40:53 +0800

    feat: 索引操作Handler 层级实现

--- ad-service/ad-search/src/main/java/com/example/ad/utils/CommonUtil.java
+++ ad-service/ad-search/src/main/java/com/example/ad/utils/CommonUtil.java
@@ -10,4 +10,14 @@ public class CommonUtil {
     public static <K, V> V getorCreate(K key, Map<K, V> map, Supplier<V> factory) {
         return map.computeIfAbsent(key, k -> factory.get());
     }
+
+    public static String stringConcat(String... args) {
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
 }
